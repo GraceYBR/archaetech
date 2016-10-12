76c090e341c69aa2818d1d175025e2f07628be7d
Tony Sun <tsun915@aucklanduni.ac.nz>
2016-10-13 10:32:33 +1300

    Use to Change Scenes
    1. Add this script to the Main camera in the hierarchy
    2. Create button for the scene you want to switch to
    3. Add the bottom of the button's object inspector, add the script
    OnClick-> Change Scne
    4. In the  Field next to the newly added function, type the name of the
    scene the button is switching to (This is because the script takes
    sceneName as a parameter
    
    Full video I used is here: https://www.youtube.com/watch?v=TYzdhiRiKd0

---
+++ Archaetech/Assets/Scripts/OnClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
+
